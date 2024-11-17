4980d665bf811b8f35c08d9e0227809857c67840
Perry Cate <thescratcherman@gmail.com>
2024-11-16 17:10:33 -0800

    clean up example

--- types.ts
+++ types.ts
@@ -1,19 +1,19 @@
 // This will be moved around once we start actually building something,
 // I'm just jotting down the types as a guiding inspiration.
 
-// Caller writes:
-type MyOutputRow = {
-  description: string
-  startDate: Date
-  quantity: number
+
+export type ImporterProps<T extends RowTemplate> = {
+  rowFormat: T
+
+  // Called once the input has been validated.
+  onSuccess: (rows: ReturnedRow<T>[]) => void
 }
 
-// Library types:
-interface RowTemplate {
+type RowTemplate = {
   [key: string]: Column<any>
 }
 
-type Column<Type> = {
+export type Column<Type> = {
   // Probably a bunch of optional hooks here for things like checking
   // the data looks a certain way and prompting the user if it doesn't.
   //
@@ -30,31 +30,33 @@ type ReturnedRow<T extends RowTemplate> = {
   [Property in keyof T]: T[Property] extends Column<infer ReturnType> ? ReturnType : never
 }
 
-// Properties for our react object.
-type ImporterProps<T extends RowTemplate> = {
-  rowFormat: T
-
-  // Called once the input has been validated.
-  onSuccess: (rows: ReturnedRow<T>[]) => void
+// A function matching this type signature will exist somewhere in the bowels of this library.
+function parse<Template extends RowTemplate>(template: Template): ReturnedRow<Template> {
+  throw "unimplemented"
 }
 
-// ---------Just testing---------.
+// ---------- Examples / Playground ----------.
 
+// Let's say a caller defines a template as such:
 const myTemplate = {
   description: {} as Column<string>,
   startDate: {} as Column<Date>,
   quantity: {} as Column<number>,
 }
 
-function parse<Template extends RowTemplate>(template: Template): ReturnedRow<Template> {
-  throw "unimplemented"
+// These are the args we'd pass into our importer react component.
+const myImporterArgs: ImporterProps<typeof myTemplate> = {
+  rowFormat: {
+    description: {} as Column<string>,
+    startDate: {} as Column<Date>,
+    quantity: {} as Column<number>,
+  },
+
+  onSuccess: (rows) => {
+    rows.map(r => {
+      console.log(r.description) // r.description is a string!
+      console.log(r.quantity * 10) // r.quantity is a number!
+      // And so forth...
+    })
+  }
 }
-
-// Holy shit it works.
-const test = parse(myTemplate)
-test.description
-test.aou
-
-myTemplate.aoeu
-
-const wat = {}
