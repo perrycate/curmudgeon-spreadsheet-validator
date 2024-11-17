accda2ea069855ca5ff9304aab479ab9aaf2c787
Perry Cate <thescratcherman@gmail.com>
2024-11-16 22:31:38 -0800

    add simple wrapper

--- src/Reviewer.tsx
+++ src/Reviewer.tsx
@@ -1,68 +1,35 @@
-import { useMemo } from 'react';
 import {
-  MRT_Table, //import alternative sub-component if we do not want toolbars
-  type MRT_ColumnDef,
+  MantineReactTable,
+  MRT_ColumnDef,
   useMantineReactTable,
 } from 'mantine-react-table';
-import { useMantineTheme } from '@mantine/core';
-import { data, type Person } from './makeData';
+import { useMemo } from 'react';
 
-export const Example = () => {
-  const { colorScheme } = useMantineTheme();
 
-  const columns = useMemo<MRT_ColumnDef<Person>[]>(
-    () => [
-      {
-        accessorKey: 'firstName',
-        header: 'First Name',
-      },
-      {
-        accessorKey: 'lastName',
-        header: 'Last Name',
-      },
-      {
-        accessorKey: 'address',
-        header: 'Address',
-      },
-      {
-        accessorKey: 'city',
-        header: 'City',
-      },
-      {
-        accessorKey: 'state',
-        header: 'State',
-      },
-    ],
-    [],
-  );
+export type ReviewerProps = {
+    columns: {key: string, name: string}[]
+    data: RawRow[]
+}
 
-  const table = useMantineReactTable({
-    columns,
-    data,
-    enableColumnActions: false,
-    enableColumnFilters: false,
-    enablePagination: false,
-    enableSorting: false,
-    mantineTableProps: {
-      highlightOnHover: false,
-      withColumnBorders: true,
-      withBorder: colorScheme === 'light',
-      sx: {
-        'thead > tr': {
-          backgroundColor: 'inherit',
-        },
-        'thead > tr > th': {
-          backgroundColor: 'inherit',
-        },
-        'tbody > tr > td': {
-          backgroundColor: 'inherit',
-        },
-      },
+export type RawRow = Record<string, number | string>
+
+export function Reviewer({
+    columns: userColumns,
+    data
+}: ReviewerProps) {
+    const columns = useMemo<MRT_ColumnDef<RawRow>[]>(() => {
+        return userColumns.map(c => ({
+            accessorKey: c.key,
+            header: c.name,
+        }))
     },
-  });
+    [userColumns],
+    )
 
-  //using MRT_Table instead of MantineReactTable if we do not want any of the toolbar features
-  return <MRT_Table table={table} />;
-};
+    const table = useMantineReactTable({
+        columns,
+        data,
+    })
 
-export default Example;
+    return <MantineReactTable table={table} />
+}
