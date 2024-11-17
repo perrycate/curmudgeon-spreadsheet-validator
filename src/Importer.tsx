2d98bdb6fe83e0bb5e197d90d02574093a3b635a
Perry Cate <thescratcherman@gmail.com>
2024-11-16 22:43:31 -0800

    stich together

--- src/Importer.tsx
+++ src/Importer.tsx
@@ -1,6 +1,8 @@
 import { ReturnedRow, RowTemplate } from "./types";
 import { configFromTemplate } from "./services/tabular-import";
 import { Mapper } from "./Mapper";
+import { useMemo, useState } from "react";
+import { Reviewer } from "./Reviewer";
 
 /**
  * Rough plan of attack:
@@ -17,23 +19,37 @@ export type ImporterProps<T extends RowTemplate> = {
   onSuccess: (rows: ReturnedRow<T>[]) => void
 }
 
+type Phase = 'mapping' | 'reviewing'
+
+type MappedData = Record<string, number | string>
+type MappedColumn = { key: string, name: string }
 
 export function Importer<T extends RowTemplate>(props: ImporterProps<T>) {
-  const config = configFromTemplate(props.columns)
+  const config = useMemo(() => configFromTemplate(props.columns), [props.columns])
 
-  return (
-    <>
-      <Mapper
-        isModal={false}
-        darkMode={true}
-        onComplete={(d) => {
-            // @ts-ignore: TODO: No type safety yet, thats' step 4.
-            props.onSuccess(d.rows.map(r => r.values))
-        }}
-        template={config}
-      />
-    </>
-  );
+  const [phase, setPhase] = useState<Phase>('mapping')
+  const [mappedColumns, setMappedColumns] = useState<MappedColumn[] | null>(null);
+  const [mappedData, setMappedData] = useState<MappedData[] | null>(null);
+
+  switch (phase) {
+    case 'mapping':
+      return (
+          <Mapper
+            isModal={false}
+            darkMode={true}
+            onComplete={(d) => {
+              setMappedColumns(d.columns)
+              setMappedData(d.rows.map(r => r.values))
+              setPhase('reviewing')
+            }}
+            template={config}
+          />
+      );
+    case 'reviewing':
+      return (mappedColumns && mappedData ?
+        <Reviewer columns={mappedColumns} data={mappedData} />
+        : null
+      )
+  }
+
 }
-
-
