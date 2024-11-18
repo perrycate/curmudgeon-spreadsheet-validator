80a53bd0c76b373c45b0aaf48e1db9e3b0424cb3
Perry Cate <thescratcherman@gmail.com>
2024-11-17 23:37:49 -0800

    dicking around with types

--- src/types.ts
+++ src/types.ts
@@ -25,10 +25,63 @@ export type Column<Type> = {
   // that type.
   //
   // (Obviously we'll need error handling etc, this is just to shut the compiler up re: unused types.)
-  parse?: (cellValue: string) => Type
+  parse: (cellValue: string) => Type
 }
+
+type MaybeOptionalVal<T, C extends Column<T>> = C extends OptionalColumn<T> ? T | null : T
 
 // ReturnedRow "unwraps" each column type in a RowTemplate.
 export type ReturnedRow<T extends RowTemplate> = {
-  readonly [Property in keyof T]: T[Property] extends Column<infer ReturnType> ? ReturnType : never
+  readonly [Property in keyof T]: T[Property] extends OptionalColumn<infer ReturnType> ? ReturnType | null : (T[Property] extends RequiredColumn<infer ReturnType> ? ReturnType : never)
+}
+
+type OptionalColumn<T> = Column<T> & {isOptional: true}
+
+type RequiredColumn<T> = (Column<T> & {isOptional: undefined | null | false}) | Omit<Column<T>, 'isOptional'>
+
+const requiredColumn: RequiredColumn<number> = {
+    parse: (_: string) => 42
+}
+
+// Let's say a caller defines a template as such:
+const myTemplate = {
+  description: {
+    isOptional: true as const,
+    parse: (_: string) => "test"
+  },
+  startDate: {
+    parse: (_: string) => new Date()
+  },
+  quantity: requiredColumn,
+}
+
+export type ImporterProps<T extends RowTemplate> = {
+  rowFormat: T
+
+  // Called once the input has been validated.
+  onSuccess: (rows: ReturnedRow<T>[]) => void
+}
+
+// These are the args we'd pass into our importer react component.
+const myImporterArgs: ImporterProps<typeof myTemplate> = {
+  rowFormat: myTemplate,
+  onSuccess: (rows) => {
+    rows.map(r => {
+      console.log(r.description) // r.description is a string!
+      console.log(r.quantity * 10) // r.quantity is a number!
+      console.log(r.startDate)
+      // And so forth...
+    })
+  }
+}
+
+function test<T>(c: Column<T>) {
+  if (c.isOptional) {
+    if (Math.random() < 0.5) {
+      return c.parse("test")
+    }
+    return null
+  }
+
+  return c.parse("test")
 }
