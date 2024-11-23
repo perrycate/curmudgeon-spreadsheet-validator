9066f9d96fca8891878c4af0954b2a6c7d9eaee3
Perry Cate <thescratcherman@gmail.com>
2024-11-23 13:51:31 -0800

    fix slow loads

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Solves the "5,000 requests on initial load" issue in dev mode.
+  // See https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
 })
