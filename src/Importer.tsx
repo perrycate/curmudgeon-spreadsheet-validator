2037cbafb0d1c1c7fa8a2452ae3d6ca2a8391a8e
Perry Cate <TheScratcherMan@gmail.com>
2025-03-09 15:25:23 -0700

    feat(dev): Push state into url for easier debugging (#4)
    * update plan
    
    * install react router
    
    * mapper
    
    * load review state from url

--- src/Importer.tsx
+++ src/Importer.tsx
@@ -1,13 +1,12 @@
 import { ReturnedRow, RowTemplate } from "./types";
-import { parseRow } from "./services/tabular-import";
 import { Mapper } from "./Mapper";
-import { useState } from "react";
-import { RawRow, Reviewer } from "./Reviewer";
+import { Route, Routes, useNavigate, useSearchParams } from "react-router-dom";
+import { Reviewer } from "./Reviewer";
 
 /**
  * Rough plan of attack:
+ * 1. Improve debugging - links to each page, pass data in url or something.
  * 2. Add some primitives for showing errors in the review phase.
- * 2.3 Improve debugging - links to each page, pass data in url or something.
  * 2.5 Figure out what primitives we need for allowing the user to actually edit/correct items during review.
  * 3. Handle optional values.
  * 4.5 Add mechanism for adding required but non-mapped columns.
@@ -19,50 +18,71 @@ export type ImporterProps<T extends RowTemplate> = {
 
   // Called once the input has been validated.
   onSuccess: (rows: ReturnedRow<T>[]) => void
+
+  // If true, routes will be set up for each individual phase, and data
+  // will be passed between phases via url params.
+  //
+  // This is probably not what we'll want when actually using this (passing data
+  // between phases via the url seems sketchy/fragile), but it should make manual
+  // testing/debugging a lot easier.
+  debugMode?: boolean
 }
 
-type Phase = 'mapping' | 'reviewing'
-
-// Matches the column key (required by the row template) to the name of the column
-// in the input file (from headers).
-type MappedColumn = { key: string, name: string }
-
 export function Importer<RT extends RowTemplate>(props: ImporterProps<RT>) {
+  const navigate = useNavigate()
 
-  const [phase, setPhase] = useState<Phase>('mapping')
-  const [_mappedColumns, setMappedColumns] = useState<MappedColumn[] | null>(null);
-  const [mappedData, setMappedData] = useState<RawRow<RT>[] | null>(null);
-
-  switch (phase) {
-    case 'mapping':
-      return (
-          <Mapper
-            template={props.columns}
-            isModal={false}
-            darkMode={true}
-            onComplete={(d) => {
-              setMappedColumns(d.columns)
-              setMappedData(d.rows.map(r => r.values))
-              setPhase('reviewing')
-
-
-              // Just testing.
-              const firstRow = Object.fromEntries(Object.entries(d.rows[0].values).map(([k, v]) => [k, v.toString()]))
-              const a = parseRow(firstRow, props.columns)
-              console.log(a)
-            }}
-          />
-      );
-    case 'reviewing':
-      if (mappedData == null) {
-        return null
-      }
-
-      return (
-        <Reviewer
-               template={props.columns}
-               data={mappedData}
-             />
-      )
+  if (!props.debugMode) {
+    return "This is dev only for now, don't actually use this. lol."
   }
+
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Mapper
+              template={props.columns}
+              isModal={false}
+              darkMode={true}
+              onComplete={(d) => {
+                const mappedData = d.rows.map(r => r.values)
+
+                navigate(`/review?data=${encodeURIComponent(JSON.stringify(mappedData))}`)
+              }}
+            />
+          }
+        />
+        <Route
+          path="/review"
+          element={
+            <ReviewerWrapper template={props.columns}/>
+          }
+        />
+        <Route path="*" element={"Route not found."} />
+      </Routes>
+    </>
+  )
+}
+
+function ReviewerWrapper<RT extends RowTemplate>({
+  template,
+}: {
+  template: RT,
+}) {
+  const [searchParams, _setSearchParams] = useSearchParams()
+
+  const encodedData = searchParams.get("data")
+  if (encodedData === null) {
+    return "No data"
+  }
+
+  const data = JSON.parse(decodeURIComponent(encodedData))
+
+  return (
+   <Reviewer
+     template={template}
+     data={data}
+   />
+  )
 }
